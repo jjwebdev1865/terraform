0f2316ef37629fbd796ac37d7905ee73f25711da
Jim Jiracek <jim.jiracek2@gmail.com>
2025-03-12 13:28:56 -0400

    creation of static website using s3 terraform configuration

--- chapter_notes/06_project_static_site.md
+++ chapter_notes/06_project_static_site.md
@@ -4,9 +4,21 @@
 - project completed in directory *proj01-s3-static-website*
 
 ## Key Notes
+- all outputs should go in a specified *outputs.tf* file
+    * outputs will been seen at the end of apply command
 
 ## Implementation Notes
 1) create provider.tf with terraform object and provider object
 2) create s3.tf file to create s3 resources
 
 - End of video one: should be able to see S3 bucket in console
+3) need to enable bucket access (done in s3.tf at "aws_s3_bucket_public_access_block")
+- End of video two: should be able to see bucket.
+    * when clicking into the bucket and selecting *Permissions*, should be able to see the bucket policy
+
+4) have a static website
+- end of video three: created a static s3 website using terraform. this was done with uploading files
+
+5) upload html files using terraform
+
+
--- proj01-s3-static-website/s3.tf
+++ proj01-s3-static-website/s3.tf
@@ -6,3 +6,61 @@ resource "aws_s3_bucket" "static_website" {
   # provides a unique id for a bucket
   bucket = "terraform-course-project-1-${random_id.bucket_suffix.hex}"
 }
+
+# NOTE: check out terraform registry about how to utilize this resource
+resource "aws_s3_bucket_public_access_block" "static_website" {
+  bucket                  = aws_s3_bucket.static_website.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# NOTE: allows public read on our static website
+resource "aws_s3_bucket_policy" "static_website_public_read" {
+  bucket = aws_s3_bucket.static_website.id
+
+  # allows for terraform to write policies for hcl
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.static_website.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.id
+
+  # When the bucket contains index.html, return this file
+  index_document {
+    suffix = "index.html"
+  }
+
+  # When the bucket doesn't contain index.html, return error document
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "index.html"
+  source       = "build/index.html"
+  etag         = filemd5("build/index.html") // NOTE: This is optional
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket       = aws_s3_bucket.static_website.id
+  key          = "error.html"
+  source       = "build/error.html"
+  etag         = filemd5("build/error.html") // NOTE: This is optional
+  content_type = "text/html"
+}
